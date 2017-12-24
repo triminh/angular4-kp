fa5d662a08c6313ede60c243013ab550c442170b
unknown <kebancung@gmail.com>
2017-12-24 16:33:19 +0700

    properties binding

--- src/app/word.component.ts
+++ src/app/word.component.ts
@@ -9,4 +9,6 @@ import { Component } from '@angular/core';
 export class WordComponent {
     en = 'Hello';
     vn = 'Xin chào';
+    imageUrl = 'https://cdn-images-1.medium.com/max/1600/1*nbJ41jD1-r2Oe6FsLjKaOg.png';
+    forgot = false;
 }
