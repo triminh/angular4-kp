21094a7a9193cf59467ecbead0a56108a3f3d7b4
unknown <kebancung@gmail.com>
2017-12-24 07:36:37 +0700

    angular cli and life cycle hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './word.component';
+import { BookComponent } from './book/book.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordComponent
+    WordComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
