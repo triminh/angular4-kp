2fdd461c9edd4ef4700bba7711eac0b11cc5422f
unknown <kebancung@gmail.com>
2018-01-22 22:48:07 +0700

    style invalid input

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -4,12 +4,24 @@ import { Component } from '@angular/core';
 selector: 'app-sign-in',
 template: `
 <form (ngSubmit)="onSubmit(formSignIn);" #formSignIn="ngForm">
-    <input placeholder="Email" ngModel name="email" required>
-    <p *ngIf="formSignIn.controls.email?.errors?.required">Email is required</p>
+    <input 
+        placeholder="Email" 
+        ngModel 
+        #txtEmail="ngModel"
+        name="email" 
+        required
+        email
+    >
+    <p *ngIf="txtEmail.touched && txtEmail.errors?.required">
+        Email is required
+    </p>
+    <p *ngIf="txtEmail.touched && txtEmail.errors?.email">
+        Email is not valid
+    </p>
     <br><br>
     <input type="password" placeholder="Password" ngModel name="password">
     <br><br>
-    <button [disabled]="formSignIn.invalid">Submit</button>
+    <button>Submit</button>
 </form>
     `
 })
