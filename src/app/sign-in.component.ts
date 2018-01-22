28a6113ff016d45ad551daf4b8d063ee6731b499
unknown <kebancung@gmail.com>
2018-01-23 00:03:47 +0700

    process checkbox and ngModelGroup

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -19,10 +19,27 @@ template: `
         Email is not valid
     </p>
     <br><br>
-    <input type="password" placeholder="Password" ngModel name="password">
+    <input 
+        type="password" 
+        placeholder="Password" 
+        ngModel
+        #txtPassword="ngModel" 
+        name="password"
+        minlength="6"
+        pattern="[a-z]*"
+        >
     <br><br>
-    <button>Submit</button>
+    <div ngModelGroup="subjects">
+        <label><input type="checkbox" [ngModel]="false" name="NodeJs">NodeJs</label>
+        <label><input type="checkbox" [ngModel]="false" name="Angular">Angular</label>
+        <label><input type="checkbox" [ngModel]="false" name="ReactJs">ReactJs</label>
+    </div>
+    <br><br>
+    <button [disabled]="formSignIn.invalid">Submit</button>
 </form>
+<p> {{ txtEmail.errors | json }} </p>
+<p> {{ txtPassword.errors | json }} </p>
+<p> {{ formSignIn.value | json }} </p>
     `
 })
 
